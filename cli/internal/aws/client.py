69cd65a6b711423f5a5bda0b315788e43d958c84
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2025-09-24 13:03:48 +0200

    feat(plan): use us-east-1 as a default region

--- cli/internal/aws/client.py
+++ cli/internal/aws/client.py
@@ -1,9 +1,9 @@
 import boto3
 
-def get_client(service_name: str, session: None) -> boto3.client:
+def get_client(service_name: str, session: None, region: None = "us-east-1") -> boto3.client:
     """Create and return a boto3 client for the specified AWS service."""
     if session:
-        return session.client(service)
+        return session.client(service_name, region)
     else:
         return boto3.client(service_name)
     
