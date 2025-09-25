82c0be5a39587f318ccfab2e4df9bfb22c85cec1
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2025-09-25 21:19:19 +0200

    feat(plan): remove code related to auth validation from planning logic

--- cli/internal/aws/plan.py
+++ cli/internal/aws/plan.py
@@ -1,21 +1,12 @@
 import boto3
 from .client import get_client
-from .session import create_session_with_profile, create_session_with_role
 from .formatter import format_rds_cluster
 from .resource_filtering import list_resource_by_tag
 
-def aws_plan(config: dict):
+def aws_plan(config: dict, session: boto3.Session):
     service = config["protect"]["resources"][0]["type"]
     region = config["provider"]["region"]
-    auth = config["auth"]
     tags = config["protect"]["resources"][0]["discover"]
-    
-    if "role_arn" in auth:
-        session = create_session_with_role(auth)
-    elif "profile" in auth:
-        session = create_session_with_profile(auth)
-    else:
-        raise ValueError("No valid authentication method found in config")
     
     client = get_client(service, session, region)
     
