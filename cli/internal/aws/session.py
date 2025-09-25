2af7f13b2fb098308403fec4779ffdf9c54c7d46
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2025-09-25 20:17:58 +0200

    feat(plan): generate a random session name for a session

--- cli/internal/aws/session.py
+++ cli/internal/aws/session.py
@@ -1,7 +1,15 @@
 import boto3
+from datetime import datetime
+
+def generate_session_name(app_name):
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    
+    return f"{app_name}-backup-{timestamp}"
 
 def create_session_with_role(auth_config: dict) -> boto3.Session:
     """Create session by assuming role"""
+    app_name = auth_config["app"]
+    
     if "profile" in auth_config:
         session = boto3.Session(profile_name=auth_config["profile"])
         sts_client = session.Client('sts')
@@ -11,7 +19,7 @@ def create_session_with_role(auth_config: dict) -> boto3.Session:
     # Assume the role
     response = sts_client.assume_role(
         RoleArn=auth_config["role_arn"],
-        RoleSessionName='backup-session' # Todo: generate a random session name
+        RoleSessionName=generate_session_name(app_name)
     )
     
     credentials = response['credentials']
@@ -25,4 +33,3 @@ def create_session_with_role(auth_config: dict) -> boto3.Session:
 def create_session_with_profile(auth_config):
     """Create session with a named profile"""
     return boto3.Session(profile_name=auth_config["profile"])
-
