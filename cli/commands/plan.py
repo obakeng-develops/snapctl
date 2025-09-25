ec5085a8b003333a519fa0f4e0f6dff9f15a2e59
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2025-09-25 20:08:16 +0200

    feat(plan): change flag to --config

--- cli/commands/plan.py
+++ cli/commands/plan.py
@@ -1,14 +1,14 @@
 import typer
 from typing_extensions import Annotated
 
-from cli.internal.config import read_config
+from cli.internal.utility.config import read_config
 from cli.internal.aws.plan import aws_plan
 
 app = typer.Typer()
 
 @app.command()
 def plan(
-    file_path: Annotated[str, typer.Option("-f", "--file", help="Config file used for defining resources")]
+    file_path: Annotated[str, typer.Option("-c", "--config", help="Config file used for defining resources")]
 ):
     """Speculatively show a plan of all the resources that will be backed up.
     """
