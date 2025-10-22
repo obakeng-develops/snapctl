047d7daec9293dca39d539535854ce35ad5782be
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2025-10-22 17:08:08 +0200

    feat(backup): organise the commands better

--- cli/commands/main.py
+++ cli/commands/main.py
@@ -1,9 +1,11 @@
 import typer
-from cli.commands.plan import app as plan_app
+from cli.commands.plan import app as plan_command
+from cli.commands.backup import app as backup_command
 
 app = typer.Typer()
 
-app.add_typer(plan_app)
+app.add_typer(plan_command)
+app.add_typer(backup_command)
 
 if __name__ == "__main__":
     app()
