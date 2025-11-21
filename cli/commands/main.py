e7577fd06eee93b4a028f74a713ad4fb40373824
Obakeng <60041842+obakeng-develops@users.noreply.github.com>
2025-11-21 13:16:18 +0200

    feat(validate): add the validate command

--- cli/commands/main.py
+++ cli/commands/main.py
@@ -1,11 +1,13 @@
 import typer
-from cli.commands.plan import app as plan_command
 from cli.commands.backup import app as backup_command
+from cli.commands.plan import app as plan_command
+from cli.commands.validate import app as validate_command
 
 app = typer.Typer()
 
-app.add_typer(plan_command)
 app.add_typer(backup_command)
+app.add_typer(plan_command)
+app.add_typer(validate_command)
 
 if __name__ == "__main__":
     app()
